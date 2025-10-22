1fb8b8fd8a48f7a743241d84f615abf21237065a
MR. TECH <rizwanthekkarakath@gmail.com>
2025-10-22 20:59:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-293949437-2531c",
+  "appId": "1:523169183481:web:cef219089f41de02719428",
+  "apiKey": "AIzaSyC_uL0nmFdLQMAZbLah5WLTKGXMICWBt5U",
+  "authDomain": "studio-293949437-2531c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "523169183481"
+};
